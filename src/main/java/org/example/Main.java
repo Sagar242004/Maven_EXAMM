6ea739e952f76b22db34c535e25b7970b2e490c8
Sagar242004 <dksagar7624910281@gmail.com>
2026-07-18 09:54:38 +0530

    First

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) throws Exception{
+        System.out.println("Hello And Welcome");
+
+        while(true){
+            System.out.println("Running");
+            Thread.sleep(5000);
+        }
+    }
+}
